2ea218afa7b56e740c450fa94f591b7338cfcc7c
Norvicdev <101652762+Norvicdev@users.noreply.github.com>
2026-03-05 22:59:32 +0300

    init

---
+++ src/main/java/ru/vicsergeev/JPager/JPagerApplication.java
@@ -0,0 +1,13 @@
+package ru.vicsergeev.JPager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vicsergeev/JPager/JPagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vicsergeev.JPager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
